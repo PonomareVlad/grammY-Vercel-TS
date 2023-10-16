8cdfdf521d88996e1875d2d21b0747b368278c7d
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-10-17 04:59:13 +0500

    Bun webhook test

--- src/webhook.ts
+++ src/webhook.ts
@@ -1,4 +1,4 @@
-import { bot } from '../src/bot'
+import { bot } from './bot'
 import { webhookCallback } from 'grammy'
 
 // webhookCallback will make sure that the correct middleware(listener) function is called
