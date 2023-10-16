0bcc51eb6981210f2232d51ad1085dad615d4be4
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-10-17 03:17:14 +0500

    cli support via deployment url

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,9 +1,9 @@
 import { bot } from '../src/bot'
 
 const {
-    VERCEL_BRANCH_URL: host,
-    // set your webhook address or use default Vercel branch url
-    WEBHOOK: webhook = `https://${host}/api`,
+    VERCEL_URL: host,
+    // set your webhook address or use default Vercel deployment url
+    WEBHOOK: webhook = `https://${host}/api/webhook`,
 } = process.env
 
 void bot.api.setWebhook(webhook)
