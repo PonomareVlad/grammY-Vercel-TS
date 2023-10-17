e4281bb2bbd9731b79ea722885b37ba5c6808f38
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-10-17 15:19:17 +0500

    Bun test

--- src/webhook.ts
+++ src/webhook.ts
@@ -4,5 +4,15 @@ import { webhookCallback } from 'grammy'
 
 // webhookCallback will make sure that the correct middleware(listener) function is called
 export default {
-    fetch: webhookCallback(bot, 'std/http'),
+    fetch: (request: Request) => {
+        console.debug(request)
+        console.debug(request.body)
+        console.debug(request.json)
+        return new Response('ok', {
+            status: 200,
+            headers: {
+                'Content-Type': 'text/plain;charset=utf-8',
+            },
+        })
+    },
 }
